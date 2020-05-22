16b0ef941a38b3a284e5513da782d44ad36ea6b3
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-21 23:55:14 -0500

    refactoring code

--- purchase_history_distribution.R
+++ purchase_history_distribution.R
@@ -1,14 +1,48 @@
+# setwd("/path/marketing_data_exploration_r/")
+
 # Importando base de datos en formato .CSV
 marketingData <- read.csv("datasets/MarketingDirecto.csv")
 
-#Obteniendo la frecuencia porcentual de la variable: Historial de compra
+# Estructura de la base de datos.
+
+str(marketingData)
+
+# Analizando la variable "Historial"
+
+# Variable "Historial" => Variable Cualitativa Ordinal
+
+# Convertiendo variable "Historial" a factor ordinal
+marketingData$Historial = factor(marketingData$Historial, levels = c("Bajo", "Medio", "Alto"), ordered = TRUE)
+
+# Obteniendo la frecuencia absoluta de la variable
+ni<-table(marketingData$Historial)
+
+# Mostrando la frecuencia absoluta de la variable
+# Bajo Medio  Alto 
+# 230   212   255 
+ni
+
+# Obteniendo la frecuencia relativa de la variable
+fi<-prop.table(table(marketingData$Historial))
+
+# Mostrando la frecuencia relativa de la variable
+# Bajo     Medio      Alto 
+# 0.3299857 0.3041607 0.3658537
+fi
+
+# Obteniendo la frecuencia porcentual de la variable
 pi<-prop.table(table(marketingData$Historial))*100
+
+# Mostrando la frecuencia porcentual de la variable
+# Bajo    Medio     Alto 
+# 32.99857 30.41607 36.58537
 pi
-# Mostrando la distribuciÃ³n de la variable: Historial de Compra
+
+# Mostrando gráfico de la distribución de la variable
 
 barplot(
   pi,
   col=c("#009E73","#E69F00","#56B4E9"),
-  main="DistribuciÃ³n de Historial de Compra", 
+  main="DistribuciÓn de la variable: Historial de Compra", 
   xlab="Historial de Compra", 
   ylab="Porcentaje de clientes")
